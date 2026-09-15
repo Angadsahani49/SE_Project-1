7900f8a327e0091ecd9b26cd10e0b92a7f373f38
Angadsahani49 <angadsahani0090@gmail.com>
2026-09-15 14:35:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  base: './',
   return {
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
